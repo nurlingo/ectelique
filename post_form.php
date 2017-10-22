b705c793b4208a4ba7099f606af788100cfdb7ed
ssydykov <saken2316@gmail.com>
2017-10-22 15:18:29 +0600

    1) index.php

--- post_form.php
+++ post_form.php
@@ -1,4 +1,10 @@
 <?php
+if( ini_get('allow_url_fopen') ) {
+        echo 'allow_url_fopen is enabled. file_get_contents should work well';    
+    } else {
+        echo 'allow_url_fopen is disabled. file_get_contents would not work';
+    }
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 if (!empty($_POST['name']) && !empty($_POST['phone'])){
   if (isset($_POST['name'])) {
